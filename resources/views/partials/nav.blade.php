22cac56533714493726cc506cc1a3e67f631f57e
justbriang <gichukxb@gmail.com>
2022-04-28 06:48:08 +0300

    add stuff at the top of the nav bar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -84,30 +84,31 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                         d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                 </svg>
             </div>
-            <div class=" pl-md-3 pl-3 text">
+            <div class=" pl-md-3 pl-3 text-center">
+                <div class="con">24/7 Customer Support</div>
                 <div class="con"><span>Call Us: </span> <span>0713-554-972 / 0724-390-322</span></div>
-                <div class="con">Call Us Now 24/7 Customer Support</div>
+
             </div>
         </div>
-        <div class="col-md-3 d-flex topper mb-md-0 mb-2 text-center">
-            <div class=" d-flex">
-                <div class="icon  ">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd"
-                            d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                            clip-rule="evenodd" />
-                    </svg>
+        <div class="col-md-3 d-flex topper mb-md-0 mb-2 justify-content-center">
 
-                </div>
-                <div class="text pl-3 pl-md-3">
-                    <div class="hr"><span>Our Location</span></div>
-                    <div class="con">Machakos County</div>
-                </div>
+            <div class="icon  ">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                        clip-rule="evenodd" />
+                </svg>
+
             </div>
+            <div class=" pl-md-3 pl-3 text-center">
+                <div class="hr"><span>Our Location</span></div>
+                <div class="con">Machakos County</div>
+            </div>
+
 
         </div>
 
-        <div class="col-md-3 d-flex  social-media justify-content-center">
+        <div class="col-md-3 d-flex  social-media justify-content-center" style="color:black">
             <p class="mb-0 d-flex  justify-content-center">
                 <a href="mailto: info@uwezowetufoundation.org"
                     class="d-flex align-items-center justify-content-center"><span class="fa fa-envelope"><i
