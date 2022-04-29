09f6f0f44426caad03e82c9e8982c01bd9d1e523
Emutisya <mutisya.elizabeth@strathmore.edu>
2022-04-29 05:54:59 +0300

    nav-links

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -2,30 +2,30 @@
     <div class="hero__slider owl-carousel">
         <div class="hero__items set-bg" data-setbg="images/main1.png" style="max-height: 70vh">
             <div class="hero__text" style="margin-top:30vh">
-                <a href="{{ Route('about') }}" class="animated-btn">Apply Now</a>
-                {{-- <a href="{{ Route('about') }}" class="primary-btn">Apply Now</a> --}}
+                <a href="https://elearning.uwezocollege.ac.ke/" target="_blank" class="animated-btn">Apply Now</a>
+                {{-- <a href="https://elearning.uwezocollege.ac.ke/" target="_blank" class="primary-btn">Apply Now</a> --}}
                 {{-- @include('partials.socials') --}}
             </div>
 
         </div>
         <div class="hero__items set-bg" data-setbg="images/uu1.jpg" style="max-height: 70vh">
             <div class="hero__text" style="margin-top:30vh">
-                <a href="{{ Route('about') }}" class="animated-btn">Apply Now</a>
-                {{-- <a href="{{ Route('about') }}" class="primary-btn">Apply Now</a> --}}
+                <a href="https://elearning.uwezocollege.ac.ke/" target="_blank" class="animated-btn">Apply Now</a>
+                {{-- <a href="https://elearning.uwezocollege.ac.ke/" target="_blank" class="primary-btn">Apply Now</a> --}}
                 {{-- @include('partials.socials') --}}
             </div>
         </div>
         <div class="hero__items set-bg" data-setbg="images/blog4.jpeg" style="max-height: 70vh">
             <div class="hero__text" style="margin-top:30vh">
-                <a href="{{ Route('about') }}" class="animated-btn">Apply Now</a>
-                {{-- <a href="{{ Route('about') }}" class="primary-btn">Apply Now</a> --}}
+                <a href="https://elearning.uwezocollege.ac.ke/" target="_blank" class="animated-btn">Apply Now</a>
+                {{-- <a href="https://elearning.uwezocollege.ac.ke/" target="_blank" class="primary-btn">Apply Now</a> --}}
                 {{-- @include('partials.socials') --}}
             </div>
         </div>
-        <div class="hero__items set-bg" data-setbg="images/uu4.png" style="max-height: 70vh">
+        <div class="hero__items set-bg" data-setbg="images/uu9.jpg" style="max-height: 70vh">
             <div class="hero__text" style="margin-top:30vh">
-                <a href="{{ Route('about') }}" class="animated-btn">Apply Now</a>
-                {{-- <a href="{{ Route('about') }}" class="primary-btn">Apply Now</a> --}}
+                <a href="https://elearning.uwezocollege.ac.ke/" target="_blank" class="animated-btn">Apply Now</a>
+                {{-- <a href="https://elearning.uwezocollege.ac.ke/" target="_blank" class="primary-btn">Apply Now</a> --}}
                 {{-- @include('partials.socials') --}}
             </div>
         </div>
